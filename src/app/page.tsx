d5fa44c61cb7b80507fb0d30b1a14eb99843e77e
Adrianodbs <adriano_alves_2008@hotmail.com>
2024-06-07 10:54:45 -0300

    Feat: Game List

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Container from "@/components/container";
+import GameCard from "@/components/gameCard";
 import Input from "@/components/input";
 import { GameProps } from "@/utils/types/game";
 import Image from "next/image";
@@ -16,9 +17,21 @@ async function getDalyGame(){
   }
 }
 
+async function getGamesData(){
+  try {
+    const res = await fetch(`${process.env.NEXT_API_URL}/next-api/?api=games`, {next: {revalidate: 320}})
+
+    return res.json()
+  } catch (error) {
+    throw new Error("Failed")
+  }
+}
+
+
 export default async function Home() {
 
   const dalyGame: GameProps = await getDalyGame()
+  const data: GameProps[] = await getGamesData()
 
   return (
     <main className="w-full">
@@ -42,6 +55,13 @@ export default async function Home() {
           </section>
         </Link>
         <Input />
+
+        <h2 className="text-lg font-bold mt-8 mb-5">Jogos para conhecer</h2>
+        <section className="grid gap-7 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+          {data.map((item)=>(
+            <GameCard key={item.id} data={item} />
+          ))}
+        </section>
 
       </Container>
     </main>
